507a24735dee948385b73c925b6837ae420afacc
Nikhil Bolar <bolar.nikhil@gmail.com>
2026-01-21 23:15:18 -0500

    Initial Commit

---
+++ WeatherKitTest/WeatherKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitTestApp.swift
+//  WeatherKitTest
+//
+//  Created by Nikhil Bolar on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
